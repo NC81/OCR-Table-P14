ed6c5877854cab51554cfeb30110d165b258c330
NC <nicolas.candeli@cegetel.net>
2023-09-09 04:33:43 +0200

    feat: add table and row components

---
+++ src/components/row.jsx
@@ -0,0 +1,15 @@
+export default function Row() {
+  return (
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  )
+}
---
+++ src/components/table.jsx
@@ -0,0 +1,19 @@
+export default function Table() {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Start Date</th>
+          <th>Department</th>
+          <th>Date of Birth</th>
+          <th>Street</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Zip Code</th>
+        </tr>
+      </table>
+    </>
+  )
+}
