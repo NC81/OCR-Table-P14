7f7f098e4a8b315252ad1ff4941d83aeb6b739c7
NC <nicolas.candeli@cegetel.net>
2023-09-09 05:57:27 +0200

    feat: display table from mocked data

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,15 +1,15 @@
-export default function Row() {
+export default function Row({ data }) {
   return (
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td>{data.firstName}</td>
+      <td>{data.lastName}</td>
+      <td>{data.startDate}</td>
+      <td>{data.department}</td>
+      <td>{data.birthDate}</td>
+      <td>{data.street}</td>
+      <td>{data.city}</td>
+      <td>{data.state}</td>
+      <td>{data.zip}</td>
     </tr>
   )
 }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,19 +1,28 @@
+import Row from './row'
+import { mockState } from '../mockState'
+
 export default function Table() {
   return (
     <>
       <table>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Start Date</th>
-          <th>Department</th>
-          <th>Date of Birth</th>
-          <th>Street</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Zip Code</th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Start Date</th>
+            <th>Department</th>
+            <th>Date of Birth</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+          </tr>
+          {mockState.map((row, index) => (
+            <Row data={row} key={`${row}-${index}`} />
+          ))}
+        </tbody>
       </table>
+      <span>Showing 1 to 10 of {mockState.length} entries</span>
     </>
   )
 }
